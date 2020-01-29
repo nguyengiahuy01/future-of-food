0be34a1c9acd5989908f4eae760e2a6f1f6334e0
Burhan Özbayram <Burhan@Burhans-MacBook-Pro.local>
2020-01-29 20:27:58 +0100

    merge

--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,8 +1,12 @@
 <template>
   <q-page class="flex flex-center">
     <div class="q-pa-md" style="max-width: 400px">
-<<<<<<< HEAD
-      <img id="loading" src="~assets/island-grey.svg" class="cute center" style="align: center" />
+      <img
+        src="~assets/island-grey.svg"
+        id="loading"
+        class="center"
+        style="width: 400px; align: center"
+      />
       <br />
       <q-form>
         <q-input
@@ -15,26 +19,17 @@
           style="min-width: 300px"
         />
         <hr />
-        <q-btn label="Jetzt spielen" class="buttonbh" @click="start()" v-if="name != ''" />
-        <q-btn label="Jetzt spielen" class="buttonbh" disabled v-else />
+        <q-btn
+          label="Jetzt spielen"
+          color="primary"
+          @click="start()"
+          v-if="name != ''"
+          style="width: 100%"
+        />
+        <q-btn label="Jetzt spielen" color="primary" disabled v-else style="width: 100%" />
       </q-form>
       <br />
     </div>
-=======
-    <img src="~assets/island-grey.svg" id="loading" class="center" style="width: 400px; align: center"><br>
-    <q-form>
-      <q-input
-        rounded outlined
-        filled v-model="name"
-        label="Dein Spielername *"
-        :rules="[ val => val && val.length > 0 || 'Wir möchten wissen, wer du bist']"
-        style="min-width: 300px"
-      /><hr>
-      <q-btn label="Jetzt spielen" color="primary" @click="start()" v-if="name != ''" style="width: 100%"/>
-      <q-btn label="Jetzt spielen" color="primary" disabled v-else style="width: 100%"/>
-    </q-form><br>
-  </div>
->>>>>>> a2cfbffcb30329dc7af17b2ce4bea5b33945b40c
   </q-page>
 </template>
 
