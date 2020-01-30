5e55d41ed5b5c248ec8e0e132fcb9814635487f4
Burhan Özbayram <Burhan@Burhans-MacBook-Pro.local>
2020-01-30 01:01:40 +0100

    Fragenthemen

--- api/database/themen.js
+++ api/database/themen.js
@@ -1,5 +1,10 @@
 module.exports = {
-  0: "wasser",
-  1: "erde",
-  2: "luft"
+  0: "SOZIOÖKONOMISCHE ENTWICKLUNG",
+  1: "VERWENDUNG VON STICKSTOFFDÜNGER",
+  2: "ERNÄHRUNG",
+  3: "ANBAU"
+  4: "TECHNOLOGIE ODER DIÄT",
+  5: "LEBENSMITTELVERSCHWENDUNG",
+  6: "WASSERNUTZUNG",
+  7: "FLEISCHKONSUM"
 };
