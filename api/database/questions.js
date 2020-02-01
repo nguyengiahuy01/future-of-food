8c85aa9bd9ae8c49fabb006451d93da06c79c7ec
Gia Huy Nguyen <nguyenggiahuy.vo@gias-mbp.speedport.ip>
2020-02-01 12:06:37 +0100

    Correct questions and themen

--- api/database/questions.js
+++ api/database/questions.js
@@ -29,67 +29,75 @@ module.exports = [
       0: "Düngen mit Stickstoffhaltigen Düngemitteln",
       1: "Gülle und Mist als Düngemittel verwenden"
     }
-    {
-      id: 4,
-      question: "Welche Ernährungsweise soll verfolgt werden?",
-      explanation: "Die Ernährungsweise der Bewohner hat große Auswirkungen auf die Insel, ... .",
-      group: 2,
-      answers: {
-        0: "Flexitarian Diet - Gemüse, Obst, Hülsenfrüchte und Nüsse",
-        1: "Guidelines Diet - inkl. tierische Produkte."
-      }
-      {
-        id: 5,
-        question: "Worauf soll auf der Insel mehr wert gelegt werden?",
-        explanation: "Die Bewohner der Insel müssen sich in einem gemeinsamen Treffen darüber einigen, in was in nächste Zeit mehr investiert werden soll.",
-        group: 4,
-        answers: {
-          0: "Verbesserung der Technologien",
-          1: "Umstellung auf eine Ernährung ohne tierische Produkte"
-        }
-        {
-          id: 6,
-          question: "Die Verschwendung von Lebensmitteln hat einen immensen Einfluss auf unsere Umwelt. Wie sollte in Zukunft damit umgegangen werden?",
-          explanation: "Durch einen Stromausfall, haben die Kühlungen der Supermärkte und der privaten Haushalte nicht funktioniert. Massen an Nahrung, vor allem Fleisch, musste weggeschmissen werden. Dies hat die Bewohner auf die generelle Lebensmittelverschwendung aufmerksam gemacht.",
-          group: 5,
-          answers: {
-            0: "Die Lebensmittelverschwendung zu minimieren ist zwar wichtig, trägt aber keinen so großen Teil zur Gesamtlage bei. Mit verschiedenen bereits bestehenden Hilfsorganisationen, die noch gutes Essen sammeln, ist schon viel getan.",
-            1: "Die Lebensmittelverschwendung sollte nochmal um mind. 50% reduziert werden. "
-          }
-          {
-            id: 7,
-            question: "Die Menschen auf der Insel haben eine weiter Anbaufläche erschaffen. Um welche Anbaufläche handelt es sich?",
-            explanation: "Bedenke: Jede Anbaufläche sollte einen gewissen Nutzen haben.",
-            group: 3,
-            answers: {
-              0: "Nahrungsmittel",
-              1: "Futtermittel"
-            }
-            {
-              id: 8,
-              question: "Die Landwirte der Insel merken, dass die Frischwassernutzung besser reguliert und effektiver genutzt werden muss, um vor allem in der Landwirtschaft Verbesserungen zu erreichen. Die Insel muss sich auf eine Kombination verschiedener Maßnahmen, bzw. Herangehensweise, einigen.",
-              explanation: "Um die Frischwassernutzung besser zu regulieren und effektiver zu nutzen, müssen sich die Bewohner der Insel auf eine Kombination verschiedener Maßnahmen einigen.",
-              group: 6,
-              answers: {
-                0: "Verbesserung der Technologien um 75% und der wie gewohnten Vermeidung von Lebensmittelverschwendung.",
-                1: "Eine weitere Reduzierung der Lebensmittelverschwendung um 50% und einem Fortschritt in der Technologie."
-              }
-              {
-                id: 9,
-                question: "Was soll auf der Insel vorzugsweise angebaut werden?",
-                explanation: "Beide Nahrungsressourcen haben verschiedene Vorteile.",
-                group: 3,
-                answers: {
-                  0: "Obst und Gemüse",
-                  1: "Hülsenfrüchte"
-              }
-              {
-                id: 10,
-                question: "Die Bewohner der Insel möchten sich einigen, einen Fleisch-Friday einzuführen. Hierbei wird gegrübelt, ob die Menschen das Fleisch aus der eigenen Tierhaltung zu besorgen, oder das Fleisch direkt importieren zu lassen.",
-                explanation: "Fleischkonsum kann einen gewissen Sättigungsgrad und Zufriedenheit der Bewohner erzielen. Jedoch sollte man auf die Auswirkungen acht geben.",
-                group: 7,
-                answers: {
-                  0: "Fleisch aus benachbarten Inseln importieren",
-                  1: "Tierzucht vergrößern um das Fleisch aus direkter Nähe zu besorgen"
-              }
-];
+  },
+  {
+    id: 4,
+    question: "Welche Ernährungsweise soll verfolgt werden?",
+    explanation: "Die Ernährungsweise der Bewohner hat große Auswirkungen auf die Insel, ... .",
+    group: 2,
+    answers: {
+      0: "Flexitarian Diet - Gemüse, Obst, Hülsenfrüchte und Nüsse",
+      1: "Guidelines Diet - inkl. tierische Produkte."
+    }
+  },
+  {
+    id: 5,
+    question: "Worauf soll auf der Insel mehr wert gelegt werden?",
+    explanation: "Die Bewohner der Insel müssen sich in einem gemeinsamen Treffen darüber einigen, in was in nächste Zeit mehr investiert werden soll.",
+    group: 4,
+    answers: {
+      0: "Verbesserung der Technologien",
+      1: "Umstellung auf eine Ernährung ohne tierische Produkte"
+    }
+  },
+  {
+    id: 6,
+    question: "Die Verschwendung von Lebensmitteln hat einen immensen Einfluss auf unsere Umwelt. Wie sollte in Zukunft damit umgegangen werden?",
+    explanation: "Durch einen Stromausfall, haben die Kühlungen der Supermärkte und der privaten Haushalte nicht funktioniert. Massen an Nahrung, vor allem Fleisch, musste weggeschmissen werden. Dies hat die Bewohner auf die generelle Lebensmittelverschwendung aufmerksam gemacht.",
+    group: 5,
+    answers: {
+      0: "Die Lebensmittelverschwendung zu minimieren ist zwar wichtig, trägt aber keinen so großen Teil zur Gesamtlage bei. Mit verschiedenen bereits bestehenden Hilfsorganisationen, die noch gutes Essen sammeln, ist schon viel getan.",
+      1: "Die Lebensmittelverschwendung sollte nochmal um mind. 50% reduziert werden. "
+    }
+  },
+  {
+    id: 7,
+    question: "Die Menschen auf der Insel haben eine weiter Anbaufläche erschaffen. Um welche Anbaufläche handelt es sich?",
+    explanation: "Bedenke: Jede Anbaufläche sollte einen gewissen Nutzen haben.",
+    group: 3,
+    answers: {
+      0: "Nahrungsmittel",
+      1: "Futtermittel"
+    }
+  },
+  {
+    id: 8,
+    question: "Die Landwirte der Insel merken, dass die Frischwassernutzung besser reguliert und effektiver genutzt werden muss, um vor allem in der Landwirtschaft Verbesserungen zu erreichen. Die Insel muss sich auf eine Kombination verschiedener Maßnahmen, bzw. Herangehensweise, einigen.",
+    explanation: "Um die Frischwassernutzung besser zu regulieren und effektiver zu nutzen, müssen sich die Bewohner der Insel auf eine Kombination verschiedener Maßnahmen einigen.",
+    group: 6,
+    answers: {
+      0: "Verbesserung der Technologien um 75% und der wie gewohnten Vermeidung von Lebensmittelverschwendung.",
+      1: "Eine weitere Reduzierung der Lebensmittelverschwendung um 50% und einem Fortschritt in der Technologie."
+    }
+  },
+  {
+    id: 9,
+    question: "Was soll auf der Insel vorzugsweise angebaut werden?",
+    explanation: "Beide Nahrungsressourcen haben verschiedene Vorteile.",
+    group: 3,
+    answers: {
+      0: "Obst und Gemüse",
+      1: "Hülsenfrüchte"
+    }
+  },
+  {
+    id: 10,
+    question: "Die Bewohner der Insel möchten sich einigen, einen Fleisch-Friday einzuführen. Hierbei wird gegrübelt, ob die Menschen das Fleisch aus der eigenen Tierhaltung zu besorgen, oder das Fleisch direkt importieren zu lassen.",
+    explanation: "Fleischkonsum kann einen gewissen Sättigungsgrad und Zufriedenheit der Bewohner erzielen. Jedoch sollte man auf die Auswirkungen acht geben.",
+    group: 7,
+    answers: {
+      0: "Fleisch aus benachbarten Inseln importieren",
+      1: "Tierzucht vergrößern um das Fleisch aus direkter Nähe zu besorgen"
+    }
+  }
+]
