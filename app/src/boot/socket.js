71c6e9f0cc0ebbfab145f28189efb88f1dcc8c74
Gia Huy Nguyen <nguyengiahuy.vo@gmail.com>
2020-04-10 09:08:37 +0200

    Update

--- app/src/boot/socket.js
+++ app/src/boot/socket.js
@@ -2,6 +2,6 @@ import io from 'socket.io-client'
 export default ({ Vue }) => {
   // Vue.prototype.$api = axios
   // const baseUri = process.env.VUE_APP_API_URL
-  Vue.prototype.$socket = io('https://future-of-food.herokuapp.com/')
-  // Vue.prototype.$socket = io('http://localhost:8085')
+  // Vue.prototype.$socket = io('https://future-of-food.herokuapp.com/')
+  Vue.prototype.$socket = io('http://localhost:8085')
 }
